9212f99b0e8effa774102f9083cbc6ccb4cc76f8
Maria Ozamiz <ozamiz.m@gmail.com>
2020-07-01 10:05:34 +0200

    add functions wink and unwink #4

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const face = document.querySelector('.face');
+const head = document.querySelector('.head');
+const page = document.querySelector('.page');
+
+function wink() {
+    face.innerHTML = ';)';
+}
+
+function unwink() {
+    face.innerHTML = ':)';
+}
+
+head.addEventListener('mouseover', wink);
+page.addEventListener('mouseout', unwink);
